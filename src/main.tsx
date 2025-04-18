9ee46704e3944703f63711c4a306a9ce5291e1f2
2020329 <jeongwoo_lee3@tmax.co.kr>
2025-04-18 23:00:12 +0900

    feat: tanstack query 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { worker } from "./mocks/browser";
 
@@ -9,10 +10,12 @@ async function bootstrap() {
   if (import.meta.env.DEV) {
     await worker.start();
   }
-
+  const queryClient = new QueryClient();
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   );
 }
