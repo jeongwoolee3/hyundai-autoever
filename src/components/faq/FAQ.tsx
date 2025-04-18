a8f8fb2adf10e7d325da6667259fa00a2649f2e3
2020329 <jeongwoo_lee3@tmax.co.kr>
2025-04-19 00:37:57 +0900

    refactor: useInfiniteQuery 사용

--- src/components/faq/FAQ.tsx
+++ src/components/faq/FAQ.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import useFAQData from "../../hooks/useFAQData";
-import { FAQItem, Tab } from "../../mocks/types";
+import { Tab } from "../../mocks/types";
 import Search from "./Search";
 import Tabs from "./Tabs";
 import CategoryFilter from "./CategoryFilter";
@@ -9,36 +9,14 @@ import FAQList from "./FAQList";
 const FAQ = () => {
   const [selectedTab, setSelectedTab] = useState<Tab>("CONSULT");
   const [selectedCategory, setSelectedCategory] = useState("");
-  const [offset, setOffset] = useState(0);
   const [question, setQuestion] = useState("");
   const [keyword, setKeyword] = useState("");
-  const [faqList, setFaqList] = useState<FAQItem[]>([]);
-  const [hasMore, setHasMore] = useState(false);
-  const limit = 10;
 
-  const { data } = useFAQData({
+  const { faqList, hasMore, fetchNextPage } = useFAQData({
     tab: selectedTab,
-    limit,
-    offset,
     faqCategoryID: selectedCategory,
     question,
   });
-
-  useEffect(() => {
-    if (!data) return;
-
-    if (offset === 0) {
-      setFaqList(data.items);
-    } else {
-      setFaqList((prev) => [...prev, ...data.items]);
-    }
-
-    setHasMore(data.items.length === limit);
-  }, [data, offset]);
-
-  useEffect(() => {
-    setOffset(0);
-  }, [selectedTab, selectedCategory]);
 
   useEffect(() => {
     setSelectedCategory("");
@@ -58,7 +36,6 @@ const FAQ = () => {
         onChange={setKeyword}
         onSearch={(question) => {
           setQuestion(question);
-          setOffset(0); // 새로운 검색이니까 첫 페이지로 초기화
         }}
       />
       {question && (
@@ -74,13 +51,12 @@ const FAQ = () => {
         selectedCategory={selectedCategory}
         onChange={(value) => {
           setSelectedCategory(value);
-          setOffset(0);
         }}
       />
       <FAQList faqList={faqList ?? []} tab={selectedTab} />
       {hasMore && (
         <button
-          onClick={() => setOffset((prev) => prev + limit)}
+          onClick={() => fetchNextPage()}
           className="cursor-pointer flex justify-center items-center w-full h-[var(--btn-xlg2)] mt-[var(--faq-list-a-size)] font-medium"
           style={{ fontSize: "var(--list-more-size)" }}
         >
--- src/hooks/useFAQData.ts
+++ src/hooks/useFAQData.ts
@@ -1,27 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { FaqResponse } from "../mocks/types";
+
+const NUM_FAQ_PER_PAGE = 10;
 
 interface UseFAQDataParams {
   tab: "CONSULT" | "USAGE";
-  limit?: number;
-  offset?: number;
   faqCategoryID?: string;
   question?: string;
 }
 
-const useFAQData = ({
-  tab,
-  limit = 10,
-  offset = 0,
-  faqCategoryID,
-  question,
-}: UseFAQDataParams) => {
-  return useQuery<FaqResponse>({
-    queryKey: ["faqList", tab, offset, faqCategoryID ?? null, question ?? null],
-    queryFn: async () => {
+const useFAQData = ({ tab, faqCategoryID, question }: UseFAQDataParams) => {
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery({
+    queryKey: ["faqList", tab, { faqCategoryID, question }],
+    queryFn: async ({ pageParam: { offset } }) => {
       const params = new URLSearchParams({
         tab,
-        limit: String(limit),
+        limit: String(NUM_FAQ_PER_PAGE),
         offset: String(offset),
       });
 
@@ -31,13 +26,23 @@ const useFAQData = ({
       if (question) {
         params.append("question", question);
       }
-      const res = await fetch(`/api/faq?${params.toString()}`);
-      if (!res.ok) throw new Error("Failed to fetch FAQ list");
-
-      return res.json();
+      const res = await axios.get(`/api/faq?${params.toString()}`);
+      return res.data as FaqResponse;
     },
-    staleTime: 1000 * 60,
-    placeholderData: undefined,
+    initialPageParam: { offset: 0 },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.pageInfo.nextOffset >= lastPage.pageInfo.totalRecord) {
+        return null;
+      }
+      return { offset: lastPage.pageInfo.nextOffset };
+    },
+    select: (data) => data.pages.flatMap((page) => page.items),
   });
+
+  return {
+    faqList: data || null,
+    hasMore: hasNextPage,
+    fetchNextPage,
+  };
 };
 export default useFAQData;
