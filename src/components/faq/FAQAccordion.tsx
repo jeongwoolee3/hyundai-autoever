d557d6332c6b77deef0b8dcbad04696744495d2c
2020329 <jeongwoo_lee3@tmax.co.kr>
2025-04-20 16:12:15 +0900

    style: 아코디언 반응형

--- src/components/faq/FAQAccordion.tsx
+++ src/components/faq/FAQAccordion.tsx
@@ -31,33 +31,40 @@ const FAQAccordion = ({ item, tab, isOpen, onToggle }: FAQAccordionProps) => {
           isOpen ? "bg-[var(--gray-10)]" : "bg-white"
         }`}
       >
-        <div></div>
         <button
           onClick={onToggle}
           type="button"
-          className="
-                cursor-pointer w-full flex items-center relative leading-[var(--line-height-sm)] py-[var(--faq-list-a-padding-v)] pr-[calc(var(--px-xlg)_+_1.6em)]"
+          className="cursor-pointer w-full flex flex-col md:flex-row items-start md:items-center md:justify-between gap-[4px] relative leading-[var(--line-height-sm)] py-[var(--faq-list-a-padding-v)] pr-[calc(var(--px-xlg)_+_1.6em)]"
           style={{ fontSize: "var(--faq-list-a-size)" }}
         >
-          {tab === "USAGE" && (
-            <em className="text-[var(--gray-400)] not-italic px-[var(--faq-list-a-padding-h)] w-[10em] ">
-              {categoryName}
+          <div className="flex">
+            {tab === "USAGE" && (
+              <div className="flex">
+                <em className="text-[var(--gray-400)] text-[calc(1em-4px)] md:text not-italic md:px-[var(--faq-list-a-padding-h)] md:w-[184px] md:inline-block md:text-[18px]">
+                  {categoryName}
+                </em>
+                <img
+                  src="/icons/ic_step_arrow.svg"
+                  width={12}
+                  className="md:hidden"
+                />
+              </div>
+            )}
+            <em className="text-[var(--gray-400)] text-[calc(1em-4px)] not-italic md:px-[var(--faq-list-a-padding-h)] md:w-[184px] md:inline-block md:text-[18px]">
+              {subCategoryName}
             </em>
-          )}
-          <em className="text-[var(--gray-400)] not-italic px-[var(--faq-list-a-padding-h)] w-[10em] ">
-            {subCategoryName}
-          </em>
+          </div>
           <strong
-            className="  flex-1 
+            className="flex-1 
                 basis-0 
-                pl-[var(--faq-list-a-padding-h)] 
+                md:pl-[var(--faq-list-a-padding-h)] 
                 text-left"
           >
             {question}
           </strong>
           <img
             src="/icons/ic_arrow.svg"
-            className="h-[var(--ic-md)] absolute right-[calc((var(--px-xlg)-var(--ic-md))/2)] text-[var(--gray-500)] transition-transform duration-300 "
+            className="h-[var(--ic-md)] absolute right-[calc((var(--px-xlg)-var(--ic-md))/2)] top-1/2 -translate-y-1/2 text-[var(--gray-500)] transition-transform duration-300 "
             style={{
               transform: isOpen ? "rotate(180deg)" : "rotate(0deg)",
             }}
