49550a5ebfe2f711318e581c69a321aff80bfef9
2020329 <jeongwoo_lee3@tmax.co.kr>
2025-04-20 16:36:04 +0900

    refactor: PageInfo 컴포넌트 분리

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import AppInfo from "./AppInfo";
 import FAQ from "./faq/FAQ";
 import FloatingButton from "./FloatingButton";
 import InquiryInfo from "./InquiryInfo";
+import PageInfo from "./PageInfo";
 import ProcessInfo from "./ProcessInfo";
 
 const Main = () => {
@@ -14,18 +15,7 @@ const Main = () => {
       }}
     >
       <div className="flex flex-col max-w-[1240px] justify-center">
-        <div
-          className="flex flex-col h-[var(--h1-height)] justify-center leading-[var(--line-height-sm)]"
-          style={{ fontSize: "var(--h1-fsize)" }}
-        >
-          <p className="font-bold text-[#05141F]">자주 묻는 질문</p>
-          <p
-            className="font-normal text-[#05141F] mt-[8px]"
-            style={{ fontSize: "var(--h1-fsize-sm)" }}
-          >
-            궁금하신 내용을 빠르게 찾아보세요.
-          </p>
-        </div>
+        <PageInfo />
         <FAQ />
         <InquiryInfo />
         <ProcessInfo />
