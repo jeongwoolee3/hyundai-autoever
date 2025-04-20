3640b18594af2a911f2a5bdcfec715ad8f3aa004
2020329 <jeongwoo_lee3@tmax.co.kr>
2025-04-20 15:43:13 +0900

    style: 앱 정보 반응형

--- src/components/AppInfo.tsx
+++ src/components/AppInfo.tsx
@@ -11,7 +11,7 @@ const AppInfo = () => {
     >
       <h2
         className="
-    text-[24px] 
+    text-[16px] sm:text-[20px] md:text-[24px]
     leading-[var(--line-height-sm)] 
     mb-[24px] 
     text-center
@@ -23,20 +23,22 @@ const AppInfo = () => {
       >
         <em className="not-italic">기아 비즈 App</em> 지금 만나보세요!
       </h2>
-      <a
-        href="https://play.google.com/store/apps/details?id=kor.mop.user.app"
-        className="flex items-center justify-center bg-white text-[var(--midnight-900)] font-semibold text-[16px] h-[60px] w-[296px] mx-[8px] rounded-[8px] lg:text-[18px] lg:h-[64px] lg:w-[392px] lg:mx-[16px]"
-      >
-        <img src="/icons/logo_googleplay.svg" alt="구글플레이" />
-        Google Play
-      </a>
-      <a
-        href="https://play.google.com/store/apps/details?id=kor.mop.user.app"
-        className="flex items-center justify-center bg-white text-[var(--midnight-900)] font-semibold text-[16px] h-[60px] w-[296px] mx-[8px] rounded-[8px] 2xl:text-[18px] 2xl:h-[64px] 2xl:w-[392px] 2xl:mx-[16px]"
-      >
-        <img src="/icons/logo_appstore.svg" alt="앱스토어" />
-        App Store
-      </a>
+      <div className="flex flex-col gap-[12px] sm:flex-row sm:gap-[16px]">
+        <a
+          href="https://play.google.com/store/apps/details?id=kor.mop.user.app"
+          className="flex items-center justify-center bg-white text-[var(--midnight-900)] font-semibold h-[60px] w-[296px] rounded-[8px] text-[14px] md:text-[16px] lg:text-[18px] lg:h-[64px] lg:w-[392px]"
+        >
+          <img src="/icons/logo_googleplay.svg" alt="구글플레이" />
+          Google Play
+        </a>
+        <a
+          href="https://play.google.com/store/apps/details?id=kor.mop.user.app"
+          className="flex items-center justify-center bg-white text-[var(--midnight-900)] font-semibold h-[60px] w-[296px] rounded-[8px] text-[14px] md:text-[16px] lg:text-[18px] 2xl:h-[64px] 2xl:w-[392px]"
+        >
+          <img src="/icons/logo_appstore.svg" alt="앱스토어" />
+          App Store
+        </a>
+      </div>
     </div>
   );
 };
